fde109307f29bc44b417c7bc221b33b24835723a
liamJonsson <limme98@gmail.com>
2026-01-19 14:24:59 +0100

    Clean up

---
+++ Models/Experience.cs
@@ -0,0 +1,10 @@
+﻿namespace Resume.Models
+{
+    public class Experience
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Period { get; set; }
+        public string Description { get; set; }
+    }
+}
