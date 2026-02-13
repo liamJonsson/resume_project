e2593782438095a0f684c06738e1aaad9989f65e
liamJonsson <limme98@gmail.com>
2026-02-13 16:17:45 +0100

    education_done?

---
+++ Models/Education.cs
@@ -0,0 +1,10 @@
+﻿namespace Resume.Models
+{
+    public class Education
+    {
+        public string Title;
+        public string School;
+        public string Years;
+        public string Description;
+    }
+}
