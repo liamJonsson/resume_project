c8c8628a9c0ef605ea36c5d59edce6e96e9b25c4
liamJonsson <limme98@gmail.com>
2026-02-20 16:25:25 +0100

    goal_all_screens

--- Models/Experience.cs
+++ Models/Experience.cs
@@ -4,7 +4,7 @@
     {
         public string Title { get; set; }
         public string Company { get; set; }
-        public string Period { get; set; }
+        public List<string> Periods { get; set; } = new List<string>();
         public string Description { get; set; }
     }
 }
